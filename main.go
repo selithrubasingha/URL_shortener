65c8ebf527de1bee6eb522077f7ffdc79ecdc984
Selith <selithrubasingha@gmail.com>
2026-08-02 23:57:37 +0530

    igniting a web server

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello Go URL Shortener !🚀" )
+	r := gin.Default()
+
+	r.GET("/",func(c *gin.Context) {
+		c.JSON(200,gin.H{
+			"message": "Hello Go URL Shortener !",
+		})
+})
+
+	err := r.Run(":9808")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+
+
 }
