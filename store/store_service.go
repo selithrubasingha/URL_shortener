ed5b788a14d2bb5be5662d0220a852b386a741fa
Selith <selithrubasingha@gmail.com>
2026-08-03 20:16:22 +0530

    feat: store service

---
+++ store/store_service.go
@@ -0,0 +1,29 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis"
+	"time"
+)
+
+
+// Define the struct wrapper around raw Redis client
+type StorageService struct {
+	redisClient *redis.Client
+}
+
+// Top level declarations for the storeService and Redis context
+var (
+	storeService  = &StorageService{}
+	ctx = context.Background()
+)
+
+
+// Note that in a real world usage, the cache duration shouldn't have  
+// an expiration time, an LRU policy config should be set where the 
+// values that are retrieved less often are purged automatically from 
+// the cache and stored back in RDBMS whenever the cache is full
+
+const CacheDuration = 6 * time.Hour
+
