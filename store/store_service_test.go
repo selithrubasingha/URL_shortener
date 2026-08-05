31996105b6b16da6a891c18c136cb9b0868ff0c1
Selith <selithrubasingha@gmail.com>
2026-08-06 00:02:12 +0530

    feat  : unit testing the store service

--- store/store_service_test.go
+++ store/store_service_test.go
@@ -1,3 +1,5 @@
+package store
+
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -7,5 +9,9 @@ var testStoreService = &StorageService{}
 
 func init() {
 	testStoreService = InitializeStore()
+}
+
+func TestStoreInit(t *testing.T) {
+	assert.NotNil(t, testStoreService.redisClient, "Redis client should not be nil after initialization")
 }
 
