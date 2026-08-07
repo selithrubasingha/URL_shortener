e270dcebcdf6edbf0c34c8439d13cc2ea995b06a
Selith <selithrubasingha@gmail.com>
2026-08-07 20:59:27 +0530

    docs : commented main file . understading gin syntax

--- main.go
+++ main.go
@@ -6,14 +6,20 @@ import (
 )
 
 func main() {
+
+	// creates a router that comes pre packages with a logger (loggin errors and stuff to the terminal)
 	r := gin.Default()
 
-	r.GET("/",func(c *gin.Context) {
+	// like the controller . when user visites "/" route , the function inside is called . 
+	r.GET("/",func(c *gin.Context) { // this c containes ALL the info of the HTTP request . very importatnt
+
+		// sends 200 request . gin.H nicely formats into JSON response
 		c.JSON(200,gin.H{
 			"message": "Hello Go URL Shortener !",
 		})
 })
 
+	// fires up built in go web server on port 9808.
 	err := r.Run(":9808")
 	if err != nil {
 		fmt.Println("Error starting server:", err)
