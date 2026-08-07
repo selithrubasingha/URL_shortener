ec3b5a18830e5b9d1f3dd97f9a2645f5efa7d9d0
Selith <selithrubasingha@gmail.com>
2026-08-07 21:33:37 +0530

    docs : commented test file . unit testing and default go tester

--- store/store_service_test.go
+++ store/store_service_test.go
@@ -5,6 +5,12 @@ import (
 	"testing"
 )
 
+
+// IMPORTANT : A test is just a script that feeds fake inputs into your
+//  functions and checks if the output matches what it expects.
+
+// go test functions in a file run top to bottom by default . but you can change it if you want . 
+
 var testStoreService = &StorageService{}
 
 func init() {
@@ -12,9 +18,13 @@ func init() {
 }
 
 func TestStoreInit(t *testing.T) {
+
+	// assert == CHECK IF ... so assert.NotNil == CHECK IF NOT NIL
 	assert.NotNil(t, testStoreService.redisClient, "Redis client should not be nil after initialization")
 }
 
+//testing methods must ALWAYS start with "Test".
+// also the file name should end with "_test.go" so that the go test command can find it and run it.
 func TestInsertAndRetrieve(t *testing.T) {
 	initialLink := "https://www.guru3d.com/news-story/spotted-ryzen-threadripper-pro-3995wx-processor-with-8-channel-ddr4,2.html"
 	userUUId := "e0dba740-fc4b-4977-872c-d360239e6b1a"
@@ -24,6 +34,7 @@ func TestInsertAndRetrieve(t *testing.T) {
 
 	retrievedUrl := RetrieveInitialUrl(shortURL)
 
+	// assert.Equal checks if the two values are equal and if not, it will fail the test and print the message provided
 	assert.Equal(t, initialLink, retrievedUrl, "The retrieved URL should match the original URL")
 	
 }
