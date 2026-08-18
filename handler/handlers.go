f52a6d49690746866e6648db0bde5982390f0141
Selith <selithrubasingha@gmail.com>
2026-08-18 23:35:52 +0530

    feat: defining the handler stubs

---
+++ handler/handlers.go
@@ -0,0 +1,19 @@
+package handler
+
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+
+func CreateShortUrl(c *gin.Context) {
+	
+    /// Implementation to be added
+
+}
+
+func HandleShortUrlRedirect(c *gin.Context) {
+
+	/// Implementation to be added
+
+}
