13800fc03d9142037f8e0c906ee4552cac8e6587
Selith <selithrubasingha@gmail.com>
2026-08-18 23:47:45 +0530

    feat: create short URL function

--- handler/handlers.go
+++ handler/handlers.go
@@ -1,11 +1,12 @@
 package handler
 
-
 import (
+	"net/http"
+
 	"github.com/eddywm/go-shortner/shortener"
 	"github.com/eddywm/go-shortner/store"
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"github.com/selithrubasingha/url-shortener/store"
 )
 
 //request model definition
@@ -16,7 +17,21 @@ type urlCreateRequest struct {
 
 func CreateShortUrl(c *gin.Context) {
 	
-    /// Implementation to be added
+    var creationRequest urlCreateRequest
+	if err := c.ShouldBindJSON(&creationRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	shortUrl, err := shortener.CreateShortUrl(creationRequest.LongUrl, creationRequest.UserId)
+	
+	store.SaveUrlMapping(shortUrl,creationRequest.LongUrl , creationRequest.UserId)
+
+	host := "http://localhost:9808/"
+	c.JSON(200, gin.H{
+		"message":   "short url created successfully",
+		"short_url": host +shortUrl,
+	})
 
 }
 
